c5d603cd4b64d932de1925d66fb66a0e267539fe
Rodolfo Silva <contato@rodolfosilva.com>
2024-03-21 22:23:03 -0300

    refactor: use regular expression to replace variables, extract keys and get attributes

--- lib/live_view/mapping/text_replacement.dart
+++ lib/live_view/mapping/text_replacement.dart
@@ -2,37 +2,34 @@ import 'package:html_unescape/html_unescape.dart';
 import 'package:liveview_flutter/live_view/state/element_key.dart';
 import 'package:xml/xml.dart';
 
-String replaceVariables(String content, Map<String, dynamic> variables) {
-  final components = Map<String, dynamic>.from(variables["c"] ?? {});
-  for (var item in variables.entries) {
-    content = content.replaceAll(
-        '<flutterState key="${item.key}"></flutterState>',
-        item.value.toString());
-    content = content.replaceAll(
-        '[[flutterState key=${item.key}]]', item.value.toString());
+const elementBracketsPattern =
+    r'\[\[flutterState\s*key=(\d+)(?:\s*component=(\d+))?\]\]';
 
-    for (var componentId in components.keys) {
-      content = content.replaceAll(
-          "[[flutterState key=${item.key} component=$componentId]]",
-          components[componentId]?[item.key]?.toString() ?? "");
+const elementXmlPattern =
+    r'<flutterState\s*key="(\d+)"(?:\s*component="(\d+)")?\s*(?:><\/flutterState|\/>)';
+
+const elementPattern = '$elementBracketsPattern|$elementXmlPattern';
+
+String replaceVariables(String content, Map<String, dynamic> variables) {
+  final components = Map<String, dynamic>.from(variables['c'] ?? {});
+
+  return content.replaceAllMapped(RegExp(elementPattern), (match) {
+    if (match.group(2) == null) {
+      return variables[match.group(1)]?.toString() ?? '';
     }
-  }
-  content = content.replaceAll(RegExp(r'\[\[flutterState key=\d+\]\]'), '');
-  return content;
+
+    return components[match.group(2)][match.group(1)].toString();
+  });
 }
 
 List<ElementKey> extractDynamicKeys(String input) {
-  var matches = RegExp(
-          r'\[\[flutterState key=(?<key>\d+)(?: component=(?<component>\d+))?\]\]')
-      .allMatches(input);
+  var matches = RegExp(elementPattern).allMatches(input);
   List<ElementKey> ret = [];
   for (var match in matches) {
-    var key = match.group(1);
-    var component = match.group(2);
-    if (key == null) {
+    if (match.group(1) == null) {
       continue;
     }
-    final elementKey = ElementKey(key, component);
+    final elementKey = ElementKey(match.group(1)!, match.group(2));
     if (!ret.contains(elementKey)) {
       ret.add(elementKey);
     }
@@ -50,25 +47,26 @@ List<ElementKey> extractDynamicKeys(String input) {
   if (attr == null) {
     return (null, null);
   }
-  var matches =
-      RegExp(r'\[\[flutterState key=(?<key>\d+)\]\]').firstMatch(attr);
-  var key = matches?.group(1);
 
-  if (key != null && variables[key] != null) {
-    var content = "${variables[key]}".trimLeft();
-
-    if (content.contains('$attribute="')) {
-      content = content.replaceFirst('$attribute="', '');
-      if (content[content.length - 1] == '"') {
-        content = content.substring(0, content.length - 1);
-      }
-    }
-    return (
-      attr.replaceAll("[[flutterState key=$key]]", content),
-      ElementKey(key)
-    );
+  var matches = RegExp(elementPattern).firstMatch(attr);
+  if (matches == null) {
+    return (attr, null);
   }
-  return (attr, null);
+
+  var elementKey = ElementKey(matches.group(1)!, matches.group(2));
+  if (elementKey.isComponent || variables[elementKey.key] == null) {
+    return (attr, null);
+  }
+
+  var content = "${variables[elementKey.key]}".replaceFirstMapped(
+    RegExp("^\\s*$attribute=\"(.*)\"\\s*\$"),
+    (m) => "${m[1]}",
+  );
+
+  return (
+    attr.replaceAll("[[flutterState key=${elementKey.key}]]", content),
+    elementKey
+  );
 }
 
 class VariableAttributes {
