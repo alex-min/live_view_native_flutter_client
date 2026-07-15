f4f9a40b20c05c9c9d19b7eda8322bb3d6c9fe69
Alexandre MINETTE <contact@alex-min.fr>
2026-07-15 11:08:56 +0200

    Add test for settings current_password error display

---
+++ test/render/settings_email_error_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:liveview_flutter/live_view/live_view.dart';
+
+import '../test_helpers.dart';
+
+main() async {
+  testWidgets('settings email form shows current_password error',
+      (tester) async {
+    var (view, _) = await connect(LiveView(), rendered: {
+      's': [
+        """
+          <flutter>
+            <AppBar>
+              <title><Text>Settings</Text></title>
+            </AppBar>
+            <viewBody>
+              <SingleChildScrollView padding="24.0">
+                <Column crossAxisAlignment="start">
+                  <Form phx-change="validate_email" phx-submit="update_email">
+                    <TextField
+                      name="user[email]"
+                      label="Email"
+                      keyboardType="emailAddress"
+                      autocorrect="false"
+                      initialValue="user@example.com"
+                      errors='[{"message": "did not change", "options": {}}]'
+                      decoration="filled: true"
+                    />
+                    <SizedBox height="16.0" />
+                    <TextField
+                      name="current_password"
+                      label="Current password"
+                      obscureText="true"
+                      autocorrect="false"
+                      enableSuggestions="false"
+                      errors='[{"message": "is not valid", "options": {}}]'
+                      decoration="filled: true"
+                    />
+                    <SizedBox height="20.0" />
+                    <ElevatedButton type="submit">Change email</ElevatedButton>
+                  </Form>
+                </Column>
+              </SingleChildScrollView>
+            </viewBody>
+          </flutter>
+        """
+      ]
+    });
+
+    await tester.runLiveView(view);
+    await tester.pumpAndSettle();
+
+    expect(find.text('is not valid'), findsOneWidget);
+  });
+}
